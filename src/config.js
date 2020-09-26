30931c12b8ae78d8fc1b355fef830e7eb8ad5a7a
andreyselin <andreiprostoandrei@gmail.com>
2020-09-26 17:57:34 +0200

    add working server

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+    twilioConfig: [
+        // Twilio Account Sid:
+        'AC02b8953b68433d779320ea356a081e91',
+        // Twilio Api Key Sid:
+        'SKcf801360d0ffa8bf39dc8f5ba92412f1',
+        // Twilio Api Key Secret:
+        'zhUrct6Z1q5Wb8DtLFmCQS6hYAGbQG3T',
+        {
+            // Max allowed session duration:
+            ttl: 14400,
+        }
+    ]
+};
